1c277c444094932861dbec5c46ada430d6a1b5d2
faissaloux <fwahabali@gmail.com>
2022-05-14 16:36:56 +0100

    downgrade laravel/framework from v9 to v8

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login');
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
