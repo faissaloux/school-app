3209dbf563d06f79af756f9aa3f155c29253d56b
faissaloux <fwahabali@gmail.com>
2022-05-18 15:04:32 +0100

    [API] authorize only parents and teachers to auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,32 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Enums\Roles;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\UserLoginRequest;
-use App\Http\Requests\Api\UserRegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(UserRegisterRequest $request)
-    {
-        $data = $request->except('_token');
-        $data['role'] = Roles::ADMIN;
-        $data['password'] = bcrypt($request->password);
-        $user = User::create($data);
-
-        $token = $user->createToken('tox_token')->plainTextToken;
-
-        $response = [
-            'user' => $user,
-            'token' => $token,
-        ];
-
-        return response()->successWithData(trans('responses.success.user.registered'), $response);
-    }
+    private $authorizedRoles = [
+        Roles::TEACHER,
+        Roles::PARENT,
+    ];
 
     public function login(UserLoginRequest $request)
     {
-        $user = User::where('email', $request->email)->first();
+        $user = User::where('email', $request->email)
+            ->whereIn('role', $this->authorizedRoles)
+            ->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->errorMessage(trans('responses.errors.wrong_credentials'));
