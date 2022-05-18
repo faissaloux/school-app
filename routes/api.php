72ac6f47933e66327a02f2e1b3f3ee55f91b3373
faissaloux <fwahabali@gmail.com>
2022-05-18 15:05:15 +0100

    [API] remove register route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
